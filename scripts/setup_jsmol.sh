eccfa634b97978b73f72830027795bac643213f3
Sean Seyler <slseyler@asu.edu>
2023-03-06 11:12:29 -0700

    Updated JSmol script to grab latest version.

--- scripts/setup_jsmol.sh
+++ scripts/setup_jsmol.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+#VERSION=14.29.31
+VERSION=16.1.5
+
 cd molecalc/static/external
 
-wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2014.29/Jmol%2014.29.31/Jmol-14.29.31-binary.zip
+#wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2014.29/Jmol%20${VERSION}/Jmol-${VERSION}-binary.zip
+wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2016.1/Jmol%20${VERSION}/Jmol ${VERSION}/Jmol-${VERSION}-binary.zip
 
-unzip Jmol-14.29.31-binary.zip
+unzip Jmol-${VERSION}-binary.zip
 
-cd jmol-14.29.31
+cd jmol-${VERSION}
 unzip jsmol.zip
 
 cd ..
 
-mv jmol-14.29.31/jsmol jsmol
+mv jmol-${VERSION}/jsmol jsmol
 
-rm -r jmol-14.29.31
-rm Jmol-14.29.31-binary.zip
+rm -r jmol-${VERSION}
+rm Jmol-${VERSION}-binary.zip
 
