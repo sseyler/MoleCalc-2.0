87a692ac9f557b8f6e227f0582694f94ed8b85d2
Sean Seyler <slseyler@asu.edu>
2024-04-24 16:39:09 -0700

    JSmol setup now uses latest JSmol version that appears to work.

--- scripts/setup_jsmol.sh
+++ scripts/setup_jsmol.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 #VERSION=14.29.31
-#VERSION=16.1.5
-VERSION=16.1.63
-#VERSION=16.2.7
+#VERSION=16.1.5   # Current version running on AWS
+#VERSION=16.1.57  # Latest version of JSmol that appears to work (mostly) as expected
+#VERSION=16.1.63  # Doesn't work (calculation "Tabs" other than Thermochemistry don't load JSmol viewer)
+#VERSION=16.2.7   # Doesn't work (calculation "Tabs" other than Thermochemistry don't load JSmol viewer)
+
+MAJOR=16
+MINOR=1
+PATCH=57
+
+VERSION=${MAJOR}.${MINOR}.${PATCH}
 
 cd molecalc/static/external
 
-#wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2014.29/Jmol%20${VERSION}/Jmol-${VERSION}-binary.zip
-#wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2016.1/Jmol%20${VERSION}/Jmol ${VERSION}/Jmol-${VERSION}-binary.zip
-wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2016.1/Jmol%20${VERSION}/Jmol-${VERSION}-binary.zip
-#wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2016.2/Jmol%20${VERSION}/Jmol-${VERSION}-binary.zip
+#wget https://sourceforge.net/projects/jmol/files/Jmol/Version%20${MAJOR}.${MINOR}/Jmol%20${VERSION}/Jmol-${VERSION}-binary.zip
+#wget https://sourceforge.net/projects/jmol/files/Jmol/Version%20${MAJOR}.${MINOR}/Jmol%20${VERSION}/Jmol ${VERSION}/Jmol-${VERSION}-binary.zip
+wget https://sourceforge.net/projects/jmol/files/Jmol/Version%20${MAJOR}.${MINOR}/Jmol%20${VERSION}/Jmol-${VERSION}-binary.zip
 
 unzip Jmol-${VERSION}-binary.zip
 
